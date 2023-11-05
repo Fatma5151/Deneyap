1c76f8ae2b130e19916b0494bedc2a08a5c8ee72
Fatma5151 <148674712+Fatma5151@users.noreply.github.com>
2023-11-05 15:13:04 +0300

    Hafta4

---
+++ Hafta4_Analiz.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int yas1,yas2,toplam;
+cout << "1. kisinin yasini giriniz:";
+cin >> yas1;
+cout << "2. kisinin yasini giriniz:";
+cin >> yas2;
+toplam = yas1+yas2;
+cout << "Toplam:";
+cout << toplam;
+return 0;
+}
---
+++ Hafta4_Kod.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int yas1,yas2,toplam;
+    yas1=12;
+    yas2=15;
+    toplam=yas1+yas2;
+    cout <<toplam;
+
+    return 0;
+}
