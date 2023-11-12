2e12071df76e1d5683ea61843b1654c5a32f2fb2
Fatma5151 <148674712+Fatma5151@users.noreply.github.com>
2023-11-12 19:29:47 +0300

    Hafta 5 kodlayıcı

---
+++ hafta 5 kodlayıcı.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sayi;
+    cout << "sayi girin \n";
+    cin >> sayi;
+    if (sayi >10)
+        cout << (sayi-10);
+    else
+        cout << (sayi+10);
+}
